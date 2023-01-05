ecb42245d6788486d84b55406e2e307488b22c8e
iamharshil <harshilc71@gmail.com>
2023-01-06 00:19:05 +0530

    prod code

---
+++ .next/server/pages/posts/[id].js
@@ -0,0 +1,77 @@
+"use strict";
+(() => {
+var exports = {};
+exports.id = 646;
+exports.ids = [646];
+exports.modules = {
+
+/***/ 270:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ Post),
+/* harmony export */   "getStaticPaths": () => (/* binding */ getStaticPaths),
+/* harmony export */   "getStaticProps": () => (/* binding */ getStaticProps)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+
+function Post({ post  }) {
+    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+        children: [
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("h1", {
+                children: "Single Post"
+            }),
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("p", {
+                children: [
+                    post.id,
+                    " ",
+                    post.body
+                ]
+            })
+        ]
+    });
+}
+async function getStaticPaths() {
+    const posts = await fetch("https://jsonplaceholder.typicode.com/posts").then((res)=>res.json());
+    const paths = posts.map((post)=>({
+            params: {
+                id: post.id.toString()
+            }
+        }));
+    return {
+        paths,
+        fallback: false
+    };
+}
+async function getStaticProps({ params  }) {
+    const post = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.id}`).then((res)=>res.json());
+    return {
+        props: {
+            post: post
+        }
+    };
+}
+
+
+/***/ }),
+
+/***/ 997:
+/***/ ((module) => {
+
+module.exports = require("react/jsx-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__(270));
+module.exports = __webpack_exports__;
+
+})();
